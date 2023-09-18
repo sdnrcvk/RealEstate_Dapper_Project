1a78ff541e5115356b5ad82f1ebc8bbf095b6d29
Sedanur Çevik <sdnrcvk@gmail.com>
2023-09-18 21:28:03 +0300

    feat : consume the api

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListViewComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListViewComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultProductListViewComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultProductListViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44315/api/Products/ProductListWithCategory");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }

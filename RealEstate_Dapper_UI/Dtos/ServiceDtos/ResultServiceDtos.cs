aadd979ef1b849cae8bc4c2a9688adb67744b279
Sedanur Çevik <sdnrcvk@gmail.com>
2023-09-20 23:49:55 +0300

    feat : consume Service api

---
+++ RealEstate_Dapper_UI/Dtos/ServiceDtos/ResultServiceDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.ServiceDtos
+{
+    public class ResultServiceDtos
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+    }
+}

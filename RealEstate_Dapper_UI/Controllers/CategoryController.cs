62b07f108eb48f20d502bb8abec8a17a5afd17dc
Sedanur Çevik <sdnrcvk@gmail.com>
2023-09-23 02:52:07 +0300

    feat : consume Category api admin panel

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.BottomGridDtos;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44315/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminNavbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.AdminLayout
+{
+    public class _AdminNavbarViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
